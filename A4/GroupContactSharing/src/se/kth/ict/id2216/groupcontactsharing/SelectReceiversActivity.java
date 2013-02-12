022168dad08d5220037d6dd49fbe92a7eb2f717b
Christoph Aldrian <aldrian@kth.se>
2013-02-12 14:59:03 +0100

    some modifications on the pages

--- A4/GroupContactSharing/src/se/kth/ict/id2216/groupcontactsharing/SelectReceiversActivity.java
+++ A4/GroupContactSharing/src/se/kth/ict/id2216/groupcontactsharing/SelectReceiversActivity.java
@@ -2,7 +2,9 @@ package se.kth.ict.id2216.groupcontactsharing;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class SelectReceiversActivity extends Activity {
 
@@ -17,6 +19,12 @@ public class SelectReceiversActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_select_receivers, menu);
 		return true;
+	}
+
+	public void onSendButton_Click(View v) {
+
+		Intent importDataIntent = new Intent(this, ImportDataActivity.class);
+		startActivity(importDataIntent);
 	}
 
 }
