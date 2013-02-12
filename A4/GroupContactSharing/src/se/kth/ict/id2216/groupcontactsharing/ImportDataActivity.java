61a175b7f607236a61a19a6c08fbed5a3adbb193
Christoph Aldrian <aldrian@kth.se>
2013-02-12 16:49:15 +0100

    programmatically add controls test

--- A4/GroupContactSharing/src/se/kth/ict/id2216/groupcontactsharing/ImportDataActivity.java
+++ A4/GroupContactSharing/src/se/kth/ict/id2216/groupcontactsharing/ImportDataActivity.java
@@ -3,6 +3,11 @@ package se.kth.ict.id2216.groupcontactsharing;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class ImportDataActivity extends Activity {
 
@@ -17,6 +22,16 @@ public class ImportDataActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_import_data, menu);
 		return true;
+	}
+
+	public void onImportButton_Click(View v) {
+
+		// add new checkboxes
+		RelativeLayout linearLayout = (RelativeLayout) findViewById(R.id.importDataLayout);
+		Button bt = new Button(this);
+		bt.setText("A Button");
+		bt.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+		linearLayout.addView(bt);
 	}
 
 }
