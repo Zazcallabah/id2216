2d2919cb73edb681f33ec916fbfed64f5d6713a4
Manuela Wüst <ella_w@gmx-topmail.de>
2013-02-13 16:28:17 +0100

    added import

--- A4/GroupContactSharing/src/se/kth/ict/id2216/groupcontactsharing/ImportDataActivity.java
+++ A4/GroupContactSharing/src/se/kth/ict/id2216/groupcontactsharing/ImportDataActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.RelativeLayout;
 
 public class ImportDataActivity extends Activity {
