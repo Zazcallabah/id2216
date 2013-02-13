eea62c5d5b9d6b1304ad2727aec3e864e3f7f860
Christoph Aldrian <aldrian@kth.se>
2013-02-13 16:32:56 +0100

    made buttons occupy the full width

--- A4/GroupContactSharing/src/se/kth/ict/id2216/groupcontactsharing/ImportDataActivity.java
+++ A4/GroupContactSharing/src/se/kth/ict/id2216/groupcontactsharing/ImportDataActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.RelativeLayout;
 
 public class ImportDataActivity extends Activity {
