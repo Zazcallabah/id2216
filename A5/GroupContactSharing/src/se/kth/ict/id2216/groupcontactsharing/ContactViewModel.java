690264022f3078aacf07dcc99e21a801a49441db
Peter Hamberg <zazcallabah@gmail.com>
2013-02-20 13:43:08 +0100

    add propertychanged handler and methods handling received contact data

--- A5/GroupContactSharing/src/se/kth/ict/id2216/groupcontactsharing/ContactViewModel.java
+++ A5/GroupContactSharing/src/se/kth/ict/id2216/groupcontactsharing/ContactViewModel.java
@@ -1,24 +1,30 @@
 package se.kth.ict.id2216.groupcontactsharing;
 
-import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-
-import org.w3c.dom.CDATASection;
-
 import android.app.Activity;
 import android.content.Context;
-
 import com.google.gson.Gson;
 
 public class ContactViewModel  {
+
+	private List<NewContactAddedListener> _listeners = new ArrayList<NewContactAddedListener>();
+	public synchronized void addEventListener(NewContactAddedListener listener)	{
+		if(! _listeners.contains(listener))
+			_listeners.add(listener);
+	}
+	public synchronized void removeEventListener(NewContactAddedListener listener)	{
+		_listeners.remove(listener);
+	}
+
 	private static String StorageLocation = "Contact_Data.json";
-	
+
 	private String name = "";
 	private String id = null;
 	private String email = "";
@@ -27,12 +33,13 @@ public class ContactViewModel  {
 	private boolean nameSelected = true;
 	private boolean emailSelected = true;
 	private boolean phoneSelected = true;
-	public List<ContactDetails> _remoteData = new LinkedList<ContactDetails>();
+	private List<ContactDetails> _remoteData;
 
 	public ContactViewModel()
 	{
+		_remoteData = new LinkedList<ContactDetails>();
 	}
-	
+
 	void setFields( ContactViewModel source )
 	{
 		this.setName(source.getName());
@@ -43,50 +50,60 @@ public class ContactViewModel  {
 		emailSelected=source.emailSelected;
 		phoneSelected=source.phoneSelected;
 	}
-	
+
 	public void save( Activity act )
 	{
 
 		try {
 			FileOutputStream fos = act.openFileOutput(StorageLocation, Context.MODE_PRIVATE);
 			fos.write(Serialize().getBytes("UTF-8"));
-		fos.close();
+			fos.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	
+
 	}
-	
+
 	public void restore( Activity act )
 	{
-			try {
-	            FileInputStream fIn;
-				fIn = act.openFileInput ( StorageLocation );
-	            JsonReader jr = new JsonReader(fIn);
-	            setFields(jr.get( ContactViewModel.class ));
-			} catch (FileNotFoundException e) {
+		try {
+			FileInputStream fIn;
+			fIn = act.openFileInput ( StorageLocation );
+			JsonReader jr = new JsonReader(fIn);
+			setFields(jr.get( ContactViewModel.class ));
+		} catch (FileNotFoundException e) {
 
-				e.printStackTrace();
-			}
+			e.printStackTrace();
+		}
 	}
-	
+
 	private static Gson _gson = new Gson();
 	public static ContactViewModel CreateViewModelFromJson( String json )
 	{
 		try{
-	 return	_gson.fromJson(json, ContactViewModel.class);
+			return	_gson.fromJson(json, ContactViewModel.class);
 		}catch(Exception e){
 			return new ContactViewModel();
 		}
 	}
-	
+
 	public String Serialize()
 	{
 		return _gson.toJson( this );
 	}
 
+	public void ForeachContact( ContactAction a )
+	{
+		synchronized(_remoteData)
+		{
+			for(ContactDetails cd : _remoteData )
+			{
+				a.run( cd );
+			}
+		}
+	}
 
 
 	public String getPhone() {
@@ -150,24 +167,54 @@ public class ContactViewModel  {
 	}
 
 	public void mergeContacts(List<ContactDetails> list) {
+		if( list == null )
+			return;
 		for(ContactDetails cd : list)
 		{
 			boolean isnew = true;
-			for( ContactDetails existing: _remoteData)
+
+			synchronized(_remoteData)
 			{
-				if( existing.id == cd.id )
+				for( ContactDetails existing: _remoteData)
 				{
-					isnew=false;
-					break;
+					if( existing.id == cd.id )
+					{
+						isnew=false;
+						break;
+					}
+				}
+				if( isnew )
+				{
+					_remoteData.add(cd);
 				}
 			}
 			if( isnew )
-			{
-				_remoteData.add(cd);
-				// fire some kind of property changed event
-			}
+				fireEventNewContactAdded(cd.id);
+
 		}
-		
+
 	}
 
+	public ContactDetails getContactById( String uuid )
+	{
+		synchronized(_remoteData)
+		{
+			for( ContactDetails cd: _remoteData)
+			{
+				if( uuid == cd.id )
+				{
+					return cd;
+				}
+			}
+		}
+		return null;		
+	}
+	private synchronized void fireEventNewContactAdded(String uuid)	{
+		NewContactEvent event = new NewContactEvent(this, uuid);
+		Iterator<NewContactAddedListener> i = _listeners.iterator();
+		while(i.hasNext())	{
+			((NewContactAddedListener) i.next()).newContactAdded(event);
+		}
+
+	}
 }
