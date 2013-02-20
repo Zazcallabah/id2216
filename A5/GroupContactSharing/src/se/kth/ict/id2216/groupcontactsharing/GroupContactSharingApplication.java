45f44db2f667340033e71cf7aabf42aa06d4a374
Peter Hamberg <zazcallabah@gmail.com>
2013-02-20 13:42:35 +0100

    clean code

--- A5/GroupContactSharing/src/se/kth/ict/id2216/groupcontactsharing/GroupContactSharingApplication.java
+++ A5/GroupContactSharing/src/se/kth/ict/id2216/groupcontactsharing/GroupContactSharingApplication.java
@@ -3,16 +3,16 @@ package se.kth.ict.id2216.groupcontactsharing;
 import android.app.Application;
 
 public class GroupContactSharingApplication extends Application {
-    @Override
-    public void onCreate() {
-        super.onCreate();
-    }
-    
-    ContactViewModel _model = null;
-    public ContactViewModel getModel()
-    {
-    	if( _model == null )
-    		_model = new ContactViewModel();
-    	return _model;
-    }
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+
+	ContactViewModel _model = null;
+	public ContactViewModel getModel()
+	{
+		if( _model == null )
+			_model = new ContactViewModel();
+		return _model;
+	}
 }
