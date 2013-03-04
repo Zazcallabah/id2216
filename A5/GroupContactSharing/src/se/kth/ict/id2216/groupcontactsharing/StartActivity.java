d6d3475bdf03656951bf38eb203ac0659bdea379
Peter Hamberg <zazcallabah@gmail.com>
2013-03-04 12:05:11 +0100

    switch back to named server

--- A5/GroupContactSharing/src/se/kth/ict/id2216/groupcontactsharing/StartActivity.java
+++ A5/GroupContactSharing/src/se/kth/ict/id2216/groupcontactsharing/StartActivity.java
@@ -11,9 +11,7 @@ import android.widget.CheckBox;
 import android.widget.TextView;
 
 public class StartActivity extends Activity {
-
-	public static final String ServerUriString = "http://130.229.162.179:8888/api/";
-	//public static final String ServerUriString = "http://tmp.prefect.se:8888/api/";
+	public static final String ServerUriString = "http://tmp.prefect.se:8888/api/";
 
 	private ContactViewModel _model;
 	private CheckBox email;
